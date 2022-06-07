d7ca212f0c558aaf7aca5258049d5de4446cb31c
buyanthg <buiantonbui"gmail.com>
2022-06-07 21:44:11 +0300

    норм

---
+++ stat/main.py
@@ -0,0 +1,17 @@
+from scipy.stats import kstest
+import matplotlib.pyplot as pl
+import pandas as p
+
+
+a = p.read_csv('stat.csv')
+pl.bar(a['Age'], a['Num'])
+a = p.DataFrame(data={'Num': a['Num']})['Num']
+
+s, p = kstest(a, (a.mean(), a.std()), 'norm')
+print('Значение p={}'.format(p))
+if p > 0.05:
+    print('Распределение норм')
+else:
+    print('эх, не повезло, не повезло')
+
+pl.show()
